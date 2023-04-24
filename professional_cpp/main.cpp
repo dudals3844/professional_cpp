0cd6b252a36e4ab2de5a65fe3cf5aab668955f37
ChoiYoungMin <dudals3844@gmail.com>
2023-04-25 00:28:33 +0900

    Initial Commit

---
+++ professional_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  professional_cpp
+//
+//  Created by 최영민 on 2023/04/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
