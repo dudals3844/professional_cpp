6890d285efb6b9ddf9eeb0b346e78cada912fa04
ChoiYoungMin <dudals3844@gmail.com>
2023-04-26 00:25:22 +0900

    add format

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <format>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    //    지금은 에러나지만 C++ 20에서 지원함
+    std::cout << format("There are {} ways", 200) << std::endl;
     return 0;
 }
