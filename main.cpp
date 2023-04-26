d0ff59a9b75b32dd3c8f86694f924e473f3629ab
ChoiYoungMin <dudals3844@gmail.com>
2023-04-27 00:09:56 +0900

    using nested namespace

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
-using std::cout;
+using namespace std;
+
+namespace First {
+    int num = 1;
+    namespace Second {
+        int num = 2;
+        namespace Third {
+            int num = 3;
+        }
+    }
+}
 
 int main() {
-    cout << "Hello World!!" << std::endl;
+    cout << First::num << endl;
+    cout << First::Second::num << endl;
+    cout << First::Second::Third::num << endl;
     return 0;
 }
