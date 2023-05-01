47af0e41d09e2fc27532f9088980e19923c3cd5f
ChoiYoungMin <dudals3844@gmail.com>
2023-05-01 21:20:38 +0900

    add struct

---
+++ employee.h
@@ -0,0 +1,15 @@
+//
+// Created by 최영민 on 2023/05/01.
+//
+
+#ifndef PROFESSIONAL_CPP__EMPLOYEE_H
+#define PROFESSIONAL_CPP__EMPLOYEE_H
+
+struct Employee{
+    char firstInitial;
+    char lastIntial;
+    int employeeNumber;
+    int salary;
+};
+
+#endif //PROFESSIONAL_CPP__EMPLOYEE_H
--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include "employee.h"
 
 using namespace std;
 
 int main() {
-    const int PieceTypeKing {0};
-    const int PieceTypeQueen {1};
-    const int PieceTypeRook {2};
-    const int PieceTypePawn {3};
 
-    int myPiece {PieceTypeKing};
+    Employee anEmployee;
+    anEmployee.firstInitial = 'J';
+    anEmployee.lastIntial = 'D';
+    anEmployee.employeeNumber = 42;
+    anEmployee.salary = 8000;
 
-    cout << PieceTypeRook << endl;
-//    상수는 변경 불가능
-//    PieceTypeRook = 3;
-
+    cout << anEmployee.firstInitial << anEmployee.lastIntial << endl;
+    cout << anEmployee.employeeNumber << endl;
+    cout << anEmployee.salary << endl;
 
     return 0;
 }
+
