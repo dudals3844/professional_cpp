9968a350c31c14165300ff0cf9bac93d3cac5865
ChoiYoungMin <dudals3844@gmail.com>
2023-05-07 20:01:42 +0900

    study three way comparisons

--- main.cpp
+++ main.cpp
@@ -3,15 +3,24 @@
 using namespace std;
 
 int main() {
-    int i{1};
-
-    if (i > 3) {
-        cout << "i > 3" << endl;
-    } else if(i >2){
-        cout << "i > 2" << endl;
-    } else{
-        cout << "hello";
+    int i{11};
+    strong_ordering result{i <=> 0};
+    if (result == strong_ordering::less) {
+        cout << "less" << endl;
     }
+    if (result == strong_ordering::greater) {
+        cout << "greater" << endl;
+    }
+
+    if (result == strong_ordering::equal) {
+        cout << "equal" << endl;
+    }
+
+    // Bolean으로 비교 가능
+    if (is_lt(result)) { cout << "less" << endl; }
+    if (is_gt(result)) { cout << "greater" << endl; }
+    if (is_eq(result)) { cout << "equal" << endl; }
+
     return 0;
 }
 
